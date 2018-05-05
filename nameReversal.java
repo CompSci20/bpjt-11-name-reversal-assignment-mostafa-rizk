a2e84cf607a1ea724baeefdf47e3224508e034a1
Mostafa Rizk <m.rizk.student@gmail.com>
2018-05-05 04:21:02 -0600

    Declared variables and asked for user input

--- nameReversal.java
+++ nameReversal.java
@@ -2,6 +2,9 @@ import java.io.*;
 import java.util.*;
 public class nameReversal {
    public static void main(String args[]) {
-      
+      Scanner kbReader = new Scanner(System.in);
+      System.out.print("Please enter your name: ");
+      String nameInput = kbReader.nextLine();
+      String nameReverse = "";
    }
 }
